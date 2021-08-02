1f92353d86d14410f9b56f822d23e5c22da3871b
nitind482 <88310869+nitind482@users.noreply.github.com>
2021-08-02 14:30:49 +0530

    adding math.py file in remote

---
+++ math.py
@@ -0,0 +1,17 @@
+#1
+def add(x,y):
+    return x+y
+#2
+def subtract(x,y):
+    return x-y #on master
+#Multipsly implementation
+def multiply(x,y):
+    return x*y # on bug456
+#4
+
+def divide(x,y):
+    if(y==0):
+		return divide by o error
+	else:
+		return x/y
+
